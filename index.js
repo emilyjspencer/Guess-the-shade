ca41a52771929d6f2f4a494c91ee714e0cb1a8aa
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-08 15:22:10 +0100

    Adds space after commas in line 18 to match the selectedColourPlayer

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const createRandomColour = () => {
     const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
-    return "rgb(" + red + "," + green + "," + blue + ")";
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
 const makeColours = (num) => {
