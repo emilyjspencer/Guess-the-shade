7fef390b0a5340afe04a97caaf22e4e48c499eb8
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-08 18:32:29 +0100

    Adds functionality whereby if the easy mode button is clicked - it turns blue. If the hard mode button is clicked - it turns blue and the easy mode button turns white again

--- index.js
+++ index.js
@@ -1,87 +1,95 @@
 
 
-const squares = document.querySelectorAll(".square");
-const colour = document.getElementById("colour")
+let squares = document.querySelectorAll(".square");
+const colourdisplay = document.getElementById("colourdisplay")
 const display = document.getElementById("winlose");
 const h1 = document.querySelector("h1");
 const resetGame = document.getElementById("resetGame");
+let easyMode = document.getElementById("easyMode");
+let hardMode = document.getElementById("hardMode");
 
-resetGame.addEventListener("click", () => {
-    
-    colours = makeColours(6);
-    
-    selectedColourComputer = computerSelectsAColour(); 
-    
-    colour.textContent = selectedColourComputer;
-    
-    for(let i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColour = colours[i];
-    }
-    console.log("clicked")
-});
 
-computerSelectsAColour = () => {
-    const random = Math.floor(Math.random() * colours.length)
-    return colours[random];
-}
-
- createRandomColour = () => {
+createRandomColour = () => {
     const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
     return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
- makeColours = (num) => {
-    const array = [];
+makeColours = (num) => {
+    let array = [];
     for(let i = 0; i < num; i++) {
         array.push(createRandomColour());
         console.log(array)
-
     }
-
     return array;
 }
+
+computerSelectsAColour = () => {
+    const random = Math.floor(Math.random() * colours.length)
+    return colours[random];
+}
+
 
 let colours = makeColours(6);
 
 let selectedColourComputer = computerSelectsAColour();
-colour.textContent = selectedColourComputer;
+colourdisplay.textContent = selectedColourComputer;
+
+resetGame.addEventListener("click", function() {
+
+    colours = makeColours(6);
+    selectedColourComputer = computerSelectsAColour();
+    colourdisplay.textContent = selectedColourComputer;
+  
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColour = colours[i];
+    }
+    h1.style.backgroundColor = "#acd1f0";
+    
+});
+  
 
 
 for(let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colours[i] 
     
-    squares[i].addEventListener("click", function(){
-        
-        console.log(this.style.backgroundColor);
+    squares[i].addEventListener("click", function() {
+
         let selectedColourPlayer = this.style.backgroundColor;
         
-        console.log(selectedColourPlayer, selectedColourComputer);
 		if(selectedColourComputer === selectedColourPlayer) {
-            console.log(selectedColourComputer);
-            console.log(selectedColourPlayer);
+
             display.textContent = "Congrats! You won"
-            changeColoursOnWin(selectedColourComputer);
-            h1.style.backgroundColor = selectedColourComputer;
+            resetGame.textContent = "Play again?";
+            changeColoursOnWin(selectedColourPlayer);
+            h1.style.backgroundColor = selectedColourPlayer;
 		} else {
-            console.log(selectedColourComputer);
-            console.log(selectedColourPlayer);
             this.style.backgroundColor = "#acd1f0";
             display.textContent = "Try again";
-		}
+        }
     });
 }
 
-const changeColoursOnWin = (colour) => {
+ changeColoursOnWin = (colour) => {
     for(let i = 0; squares.length; i++) {
         squares[i].style.backgroundColor = colour;
     }
 }
 
-const clickToMakeNewColours = () => {
+easyMode.addEventListener("click", () => {
+    hardMode.classList.remove("mode");
+    easyMode.classList.add("mode");
+});
 
-}
+hardMode.addEventListener("click", () => {
+    hardMode.classList.add("mode");
+    easyMode.classList.remove("mode");
+    
+});
+
+
+
 
 
 
