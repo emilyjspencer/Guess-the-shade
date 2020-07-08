6a5394ddd343ae1690ab562549b6a4488727bb86
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-08 13:37:50 +0100

    Adds a randomizing function. The colour selected by the computer changes on each page refresh

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const colours = ["rgb(200, 200, 4)", "rgb(100, 234, 32)", "rgb(32, 210, 39)", "rgb(255, 80, 146)", "rgb(120, 56, 2)", "rgb(210, 30, 4)", "rgb(2, 229, 250)", "rgb(150, 21, 89)"]
 
+
+const computerSelectsAColour = () => {
+    const random = Math.floor(Math.random() * colours.length)
+    return colours[random];
+}
+
+
 const squares = document.querySelectorAll(".square");
-const selectedColourComputer = colours[2];
+const selectedColourComputer = computerSelectsAColour();
 
 const colour = document.getElementById("colour")
 const display = document.getElementById("winlose");
@@ -31,8 +38,9 @@ for(let i = 0; i < squares.length; i++) {
 }
 
 
-changeColoursOnWin = (colour) => {
+const changeColoursOnWin = (colour) => {
     for(let i = 0; squares.length; i++) {
         squares[i].style.backgroundColor = colour;
     }
 }
+
